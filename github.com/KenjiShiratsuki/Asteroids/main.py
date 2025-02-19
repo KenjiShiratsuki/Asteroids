c7ad18c1911076edd49569ab6b818fb2a5008a3d
KenjiShiratsuki <kenjishiratsuki@gmail.com>
2025-02-19 18:07:05 -0500

    Player working

--- github.com/KenjiShiratsuki/Asteroids/main.py
+++ github.com/KenjiShiratsuki/Asteroids/main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from circleshape import CircleShape
+from player import Player
 def main():
     pygame.init()
     clock = pygame.time.Clock()
@@ -8,11 +10,13 @@ def main():
     print(SCREEN_WIDTH)
     print(SCREEN_HEIGHT)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x=SCREEN_WIDTH/2,y=SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
         clock.tick(60)
 if __name__ == "__main__":
