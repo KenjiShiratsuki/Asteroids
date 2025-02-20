9e166b63b0ef9b33d75e291a450a8564c7ef2193
KenjiShiratsuki <kenjishiratsuki@gmail.com>
2025-02-20 15:40:14 -0500

    Player rotation added, dt fixed

--- github.com/KenjiShiratsuki/Asteroids/main.py
+++ github.com/KenjiShiratsuki/Asteroids/main.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame # type: ignore
 from constants import *
 from circleshape import CircleShape
 from player import Player
@@ -16,8 +16,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
-        clock.tick(60)
+        dt = clock.tick(60)/1000
 if __name__ == "__main__":
     main()
