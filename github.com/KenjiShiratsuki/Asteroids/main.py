fd0fb94c7b2b90fa2c01d1276fcc8281ff71fc64
KenjiShiratsuki <kenjishiratsuki@gmail.com>
2025-02-20 16:27:06 -0500

    created asteroids and asteroidfield

--- github.com/KenjiShiratsuki/Asteroids/main.py
+++ github.com/KenjiShiratsuki/Asteroids/main.py
@@ -2,6 +2,8 @@ import pygame # type: ignore
 from constants import *
 from circleshape import CircleShape
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import *
 def main():
     pygame.init()
     clock = pygame.time.Clock()
@@ -10,14 +12,22 @@ def main():
     print(SCREEN_WIDTH)
     print(SCREEN_HEIGHT)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    field = AsteroidField()
     player = Player(x=SCREEN_WIDTH/2,y=SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        screen.fill("black")
+        for obj in drawable:   
+            obj.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 if __name__ == "__main__":
